814f3c9adfd9d27fba6cd926c3c689ae4e0b686a
DM <djmedora@gmail.com>
2017-10-07 16:21:34 -0700

    added capitalone api calls

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import BillList from './components/billList';
 import BillPage from './components/billPage';
-import Launch from './components/launch'
 import appState from './stores/appState';
 import { Provider } from 'mobx-react/native';
 import { Router, Scene } from 'react-native-router-flux';
+import {
+    chargeCustomerAccount, getAllBillsForCustomer, getAccountsOfCustomer,
+    getSpecificBill, getAllDonationReceiptsForCustomer, createDonationReceiptForAccount,
+    createPlaceholderRepresentativeAccount, createPurchaseForAccount, getPurchasesToMerchantForAccount
+} from "./services/transport-layer";
 
 export default class App extends React.Component {
 
     componentDidMount = async () => {
-        await appState.getBills()
+        await appState.getBills();
+        // await getSpecificBill("https://api.propublica.org/congress/v1/115/bills/s1798.json");
+        // await createDonationReceiptForAccount("59d8688ba73e4942cdafdf84", "Daryus", 20);
+        // console.log("about to initiate function");
+        // await chargeCustomerAccount("59d8688ba73e4942cdafdf84", "", 1, "test txn");
+        // await getAccountsOfCustomer("59d8688aa73e4942cdafdf83");
+        // // await getAllDonationReceiptsForCustomer("59d8688ba73e4942cdafdf84");
+        // // await createPlaceholderRepresentativeAccount();
+        await createPurchaseForAccount("59d8688ba73e4942cdafdf84", 3, representative_name="test_txn");
+        // await getAccountsOfCustomer("59d8688aa73e4942cdafdf83");
+        await getPurchasesToMerchantForAccount("59d8688ba73e4942cdafdf84");
+
     };
 
     render() {
@@ -17,7 +32,6 @@ export default class App extends React.Component {
             <Provider appState={appState}>
                 <Router>
                     <Scene key="root" hideNavBar hideTabBar>
-                        <Scene navigationBarStyle={{statusBarTextColorScheme: 'light'}} key="launch" component={Launch}/>
                         <Scene navigationBarStyle={{statusBarTextColorScheme: 'light'}} key="billList" component={BillList}/>
                         <Scene navigationBarStyle={{statusBarTextColorScheme: 'light'}} key="billPage" component={BillPage}/>
                     </Scene>
